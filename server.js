b99532dd101cf082fa34f7c80ebbd482a5b6a80f
Jassim Hayat <jassim.hayat@gmail.com>
2025-01-10 16:12:46 +0300

    Skyrockit

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ const app = express();
 const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
+// server.js
+
+const applicationsController = require('./controllers/applications.js');
+
+// require our new middleware!
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
 
 // CONTROLLERS
 const authCtrl = require('./controllers/auth');
@@ -29,14 +36,27 @@ app.use(
   })
 );
 
+app.use(passUserToView); // use new passUserToView middleware here
+
+
+
 // PUBLIC ROUTES
 app.get('/', (req, res) => {
-  res.render('index.ejs', {
-    user: req.session.user,
-  });
+  // Check if the user is signed in
+  if (req.session.user) {
+    // Redirect signed-in users to their applications index
+    res.redirect(`/users/${req.session.user._id}/applications`);
+  } else {
+    // Show the homepage for users who are not signed in
+    res.render('index.ejs');
+  }
 });
 
 app.use('/auth', authCtrl);
+app.use(isSignedIn); // use new isSignedIn middleware here
+app.use('/users/:userId/applications', applicationsController); // New!
+
+
 
 // PROTECTED ROUTES
 app.get('/vip-lounge', (req, res) => {
@@ -46,6 +66,9 @@ app.get('/vip-lounge', (req, res) => {
     res.send('Sorry, no guests allowed.');
   }
 });
+
+
+
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
